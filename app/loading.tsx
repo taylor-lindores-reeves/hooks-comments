10de912fbba3383342d45aa55b20ccca97842bae
Taylor Lindores Reeves <taylor-lindores@hotmail.co.uk>
2023-11-13 18:18:50 +0000

    Remove the loading component

--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-import { CommentsLoading } from "@hooks-comments/components/comments-loading";
-import React from "react";
-
-const Loading = () => {
-	return <CommentsLoading />;
-};
-
-export default Loading;
